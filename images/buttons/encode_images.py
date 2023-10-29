700f415ac49694b394eee61df1d7d3e25b7d7637
brasno <brasno@gmail.com>
2023-10-29 23:56:08 +0100

    Created images for buttons. encode_images.py generates images.py, encoded images for copying.

---
+++ images/buttons/encode_images.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+import os
+import base64
+
+def main():
+    OUT = 'images.py'
+    dir='.'
+    images = [f for f in os.listdir(dir) if f.endswith('.png') or f.endswith('.ico') or f.endswith('.gif')]
+
+    outfile = open(os.path.join(dir, OUT), 'w')
+
+    for i, file in enumerate(images):
+        image = open(os.path.join(dir, file), 'rb').read()
+        encoded = base64.b64encode(image)
+        outfile.write('{} = {}\n'.format(file[:file.index(".")], encoded))
+
+    outfile.close()
+    print('{} images encoded.'.format((i+1)))
+
+if __name__ == '__main__':
+    main()
